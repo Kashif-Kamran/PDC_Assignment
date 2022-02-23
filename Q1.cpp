d05498a912cb22de2e5b52d75105aa825726d056
Kashif-Kamran <i190541@nu.edu.pk>
2022-02-23 19:25:55 +0500

    Question 1, First Version Has been Completed

--- Q1.cpp
+++ Q1.cpp
@@ -2,6 +2,66 @@
 #include <stdlib.h>
 #include <time.h>
 using namespace std;
+float **serialMultiplication(float **mat1, float **mat2, int row, int col);
+void assignRendomValues(float **&matrix, int row, int col, int range);
+void allocateDynamicMemory(float **&matrix, int row, int col);
+void displayMatrix(float **matrix1, int row, int col);
+int main()
+{
+    /* -------------------------------------------------------------------------- */
+    /*                                Preprocessing                               */
+    /* -------------------------------------------------------------------------- */
+    int row;
+    int col;
+
+    float **matrix1 = NULL;
+    float **matrix2 = NULL;
+    float **resultantMatrix = NULL;
+    /* -------------------- Initilize Dimentions of Matrix 1 -------------------- */
+    cout << "===== Enter Dimentations of Matrixes=====" << endl;
+    srand(time(NULL));
+    cout << "Enter Dimention Of Matrix : ";
+    cin >> row;
+    col = row;
+    /* ------------------ Allocating Dynamic Memory ofMatrixes ------------------ */
+    allocateDynamicMemory(matrix1, row, col);
+    allocateDynamicMemory(matrix2, row, col);
+    /* ----------------------------- Displaying Data ---------------------------- */
+    assignRendomValues(matrix1, row, col, 10);
+    assignRendomValues(matrix2, row, col, 10);
+    cout << "Matrix 1 : " << endl;
+    displayMatrix(matrix1, row, col);
+    cout << "Matrix 2 : " << endl;
+    displayMatrix(matrix2, row, col);
+    resultantMatrix = serialMultiplication(matrix1, matrix2, row, col);
+    cout << "Resultant Matrix : " << endl;
+    displayMatrix(resultantMatrix, row, col);
+    return 0;
+}
+float **serialMultiplication(float **mat1, float **mat2, int row, int col)
+{
+    float **resultant = NULL;
+    allocateDynamicMemory(resultant, row, col);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            resultant[i][j] = 0;
+
+            for (int k = 0; k < row; k++)
+            {
+                resultant[i][j] += mat1[i][k] * mat2[k][j];
+            }
+
+            cout << resultant[i][j] << "\t";
+        }
+
+        cout << endl;
+    }
+    displayMatrix(resultant, row, col);
+    return resultant;
+}
 void assignRendomValues(float **&matrix, int row, int col, int range = 10)
 {
     for (int i = 0; i < row; i++)
@@ -20,7 +80,7 @@ void allocateDynamicMemory(float **&matrix, int row, int col)
         matrix[i] = new float[col];
     }
 }
-void displaymatrix1(float **matrix1, int row, int col)
+void displayMatrix(float **matrix1, int row, int col)
 {
     for (int i = 0; i < row; i++)
     {
@@ -30,49 +90,4 @@ void displaymatrix1(float **matrix1, int row, int col)
         }
         cout << endl;
     }
-}
-int main()
-{
-    /* -------------------------------------------------------------------------- */
-    /*                                Preprocessing                               */
-    /* -------------------------------------------------------------------------- */
-    int row1;
-    int col1;
-    int row2;
-    int col2;
-
-    float **matrix1 = NULL;
-    float **matrix2 = NULL;
-    /* -------------------- Initilize Dimentions of Matrix 1 -------------------- */
-    cout << "===== Enter Dimentations of Matrix 1 =====" << endl;
-    srand(time(NULL));
-    cout << "Enter Rows Number : ";
-    cin >> row1;
-    cout << "Enter Column Number  ";
-    cin >> col1;
-    /* ------------------- Initialize Dimentations of Matrix 2 ------------------ */
-    cout << "===== Enter Dimentations of Matrix 2 =====" << endl;
-    srand(time(NULL));
-    cout << "Enter Rows Number : ";
-    cin >> row2;
-    cout << "Enter Column Number  ";
-    cin >> col2;
-    /* ------------- Check multiplication Competibility of Matrixes ------------- */
-    if (col1 != row2)
-    {
-        cout << "\t\t\tError:->   These Matrixes Are Not Competible For Multiplication" << endl;
-        return 0;
-    }
-    /* ------------------ Allocating Dynamic Memory ofMatrixes ------------------ */
-    allocateDynamicMemory(matrix1, row1, col1);
-    allocateDynamicMemory(matrix2, row2, col2);
-    /* ----------------------------- Displaying Data ---------------------------- */
-
-    int **resultantMatrix = NULL;
-    assignRendomValues(matrix1, row1, col1);
-    cout << "Before Assiging Values : " << endl;
-    displaymatrix1(matrix1, row1, col1);
-    cout << "After Assigning the Values : " << endl;
-    displaymatrix1(matrix1, row1, col1);
-    return 0;
 }
